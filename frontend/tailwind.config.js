dfaba1261e2c705cb7760175dfc07a70b69504f4
ThulyaRodrigo <133916791+ThulyaRodrigo@users.noreply.github.com>
2026-03-10 16:34:42 +0530

    chore: setup global tailwindcss theme with official colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sliit: {
+          blue: '#053668',
+          orange: '#FF7100',
+          yellow: '#F7ECB5',
+        },
+      },
+    },
   },
   plugins: [],
 }
