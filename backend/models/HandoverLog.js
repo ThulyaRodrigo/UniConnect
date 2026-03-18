d2f92a95b4db7d0cad75a2838b3bbccbe381f71b
kalana1999 <202782806+kalana1999@users.noreply.github.com>
2026-03-18 14:47:56 +0530

    feat: Implement HandoverLog model

---
+++ backend/models/HandoverLog.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const handoverLogSchema = new mongoose.Schema({
+    society: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Society', 
+        required: true 
+    },
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    action: { 
+        type: String, 
+        enum: ['Promoted to Admin', 'Demoted to Student'], 
+        required: true 
+    },
+    performedBy: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('HandoverLog', handoverLogSchema);
